c217c8692f062e4966904d4b5a3bf208843d3a94
Gian Carlo Orcotoma Mormontoy <giancarlozklm2015@gmail.com>
2024-12-08 19:22:08 -0500

    Feat: Agragando logging para cargar enaho

--- src/data/cargar_enaho.py
+++ src/data/cargar_enaho.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import sys
 
 # Ruta principal donde buscar las carpetas y archivos CSV
 path = "datasets/ENAHO"
@@ -23,6 +24,7 @@ def cargar_enaho():
             # Iterar sobre los archivos dentro de la carpeta
             for file_name in os.listdir(folder_path):
                 if file_name.endswith(".csv"):
+                    sys.stderr.write(f"[cargar_enaho.py] 📊 Leyendo archivo {os.path.join(folder_path, file_name)}")
                     file_path = os.path.join(folder_path, file_name)
                     try:
                         # Leer el archivo CSV
